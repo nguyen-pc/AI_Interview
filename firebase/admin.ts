37cd5f6d7f98d86b73e82d104e14b4af1b9a8b54
nguyen <nguyenthcs430@gmail.com>
2025-04-12 13:49:22 +0700

    implment auth and store

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -4,12 +4,6 @@ import { getFirestore } from "firebase-admin/firestore";
 // import { auth_provider_x509_cert_url } from "../prewise-fbf5c-firebase-adminsdk-fbsvc-d6ff899641.json";
 
 const initFirebaseAdmin = () => {
-  console.log(
-    "Resolved key:\n",
-    process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
-  );
-  console.log(process.env.FIREBASE_PROJECT_ID);
-  console.log(process.env.FIREBASE_CLIENT_EMAIL);
   const apps = getApps();
   if (!apps.length) {
     initializeApp({
